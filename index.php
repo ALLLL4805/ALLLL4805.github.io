323c214a4c246879e4d03cfbb989cf631e0a1b6a
ALLLL4805 <114454284+ALLLL4805@users.noreply.github.com>
2022-09-27 09:16:52 +0700

    al

---
+++ index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Project PKK</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body style="background-color:grey;">
+<table style="width: 100%; height: 100%;"; border="0px"; align="center">
+      <tr style="background-color:white;  box-shadow: 2px 7px 11px blue;  ">
+        <td style="width: 15%"> 
+          <font style="font-style: italic; font-size: 72px;font-family:roboto,lucon,sans-serif; text-shadow:5px 0px 2px;margin-left: 20px;"></font> 
+           
+          <font style="font-style:d; font-size: 72px;font-family:roboto,lucon,sans-serif;text-shadow: -5px 0px 2px blue; "><a href="#"  style="color: black; text-decoration: none" >Wibuuu</a></font>
+        </td>
+        <td>&nbsp</td>
+        <td style="width: 0%; text-align: right">
+          <div class="dropdown"> 
+            <button class="button1">HOME</button>
+            <div class="dropdown-content" style="text-align: left;">
+              
+              <a href="#" target="_blank">Menu 1</a>
+              <a href="#" target="_blank">Menu 2</a>
+              <a href="#" target="_blank">Menu 3</a>
+              <a href="#" target="_blank">Games</a>
+            </div>
+          </div>
+          </a>
+        </td>
+        <td style="width: 0%; text-align: right">
+          <div class="dropdown"> 
+            <button class="button1">MENU</button>
+            <div class="dropdown-content" style="text-align: left;">
+              
+             <a href="#" target="_blank">Menu 1</a>
+             <a href="#" target="_blank">Menu 2</a>
+             <a href="#" target="_blank">Menu 3</a>
+             <a href="#" target="_blank">Games</a>
+
+            </div>
+          </div>
+          </a>
+        </td>
+        <td style="width: 5%; text-align: center">
+          <a href="product.php" ; style="color: black; text-decoration: none;font-family: 'Courier New', Courier, monospace;"  >
+
+            <div class="dropdown"> 
+            <button class="button1">PRODUCT</button>
+            <div class="dropdown-content" style="text-align: left;">
+              
+             <a href="#" target="_blank">Menu 1</a>
+             <a href="#" target="_blank">Menu 2</a>
+             <a href="#" target="_blank">Menu 3</a>
+             <a href="#" target="_blank">Games</a>
+
+            </div>
+          </div>
+          </a>
+      </tr>
+    </table>
+<h2 style="color : white;" align = "center"> Nama Kelompok </h2>
+<table border="1px" align = "center" style="background-color:white ;">
+    <tr class="oi">
+        <th>FOTO</th>
+        <th>NAMA</th>    
+        <th>TTL</th>
+        <th>ALAMAT</th>
+    </tr>
+    <tr class = "ih">
+        <td><img src="aldi.jpeg" width="200px" height="201px">
+        <td align="center">Aldi Rudiansyah</td>
+        <td>Tasikmalaya,4 Agustus 2005</td>
+        <td>Kp.Bojognangka</td>
+
+    </tr>
+    <tr class = "ah">
+        <td><img src="ghazia.png" width="200px" height="200px"></td>
+        <td>Ghazia Zidane Octavarian</td>
+        <td>Tasikmalaya,19 Oktober 2006</td>
+        <td>Kp.Cigantang</td>
+    </tr>
+    <tr class = "eh">
+        <td><img src="ricky.jpeg" width="200px" height="200px"></td>
+        <td>Ricky Kurniawan Alamsyah</td>
+        <td>Tasikmalaya,31 Desember 2005</td>
+        <td>Kp.Bebedahan</td>
+    </tr>
+
+
+</table>
+</body>
+</html>
