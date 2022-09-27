2fbf199021552526a1789ff821f7951618e72db4
ALLLL4805 <114454284+ALLLL4805@users.noreply.github.com>
2022-09-27 09:24:48 +0700

    asd

---
+++ baubawang/product.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Project PKK</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body style="background-color:grey;">
+<table style="width: 100%; height: 100%;"; border="0px"; align="center">
+      <tr style="background-color:white;  box-shadow: 2px 7px 11px blue;  ">
+        <td style="width: 15%"> 
+          <font style="font-style: italic; font-size: 72px;font-family:roboto,lucon,sans-serif; text-shadow:5px 0px 2px;margin-left: 20px;"></font> 
+           
+          <font style="font-style:d; font-size: 72px;font-family:roboto,lucon,sans-serif;text-shadow: -5px 0px 2px blue; "><a href="#"  style="color: black; text-decoration: none" >Wibuuu</a></font>
+        </td>
+        <td>&nbsp</td>
+        <td style="width: 0%; text-align: right">
+        <a href="index.php" ; style="color: black; text-decoration: none;font-family: 'Courier New', Courier, monospace;"  >
+          <div class="dropdown"> 
+            <button class="button1">HOME</button>
+            <div class="dropdown-content" style="text-align: left;">
+              
+              <a href="#" target="_blank">Menu 1</a>
+              <a href="#" target="_blank">Menu 2</a>
+              <a href="#" target="_blank">Menu 3</a>
+              <a href="#" target="_blank">Games</a>
+            </div>
+          </div>
+          </a>
+        </td>
+        <td style="width: 0%; text-align: right">
+          <div class="dropdown"> 
+            <button class="button1">MENU</button>
+            <div class="dropdown-content" style="text-align: left;">
+              
+             <a href="#" target="_blank">Menu 1</a>
+             <a href="#" target="_blank">Menu 2</a>
+             <a href="#" target="_blank">Menu 3</a>
+             <a href="#" target="_blank">Games</a>
+
+            </div>
+          </div>
+          </a>
+        </td>
+        <td style="width: 5%; text-align: center">
+          <a href="about.html" ; style="color: black; text-decoration: none;font-family: 'Courier New', Courier, monospace;"  >
+
+            <div class="dropdown"> 
+            <button class="button1">PRODUCT</button>
+            <div class="dropdown-content" style="text-align: left;">
+              
+             <a href="#" target="_blank">Menu 1</a>
+             <a href="#" target="_blank">Menu 2</a>
+             <a href="#" target="_blank">Menu 3</a>
+             <a href="#" target="_blank">Games</a>
+
+            </div>
+          </div>
+          </a>
+      </tr>
+    </table>
+
+<center>
+<h2 style="color : white;">PRODUCT</h2>
+
+<table border="1px" style="background-color:blue;">
+    <tr class="oi">
+        <th>FOTO</th>
+        <th>NAMA</th>    
+        <th>HARGA</th>
+        <th>DESKRIPSI</th>
+    </tr>
+    <tr class = "ih" align          = "center">
+        <td><img src="bakso.jpeg" width="200px" height="201px"></td>
+        <td>Bakso Beranak</td>
+        <td>Rp.15.000,00</td>
+        <td>Bakso yang berisikan 4 buah bakso kecil</td>
+    </tr>
+        <tr>
+          <td colspan="4"  align = "center"> <button  style="background-color:pink; color:black;" > ORDER</button> </td>
+        </tr>
+    <tr class = "ah" align = "center">
+        <td><img src="mie_ayam.jpeg" width="200px" height="200px"></td>
+        <td>Mie Ayam</td>
+        <td>Rp.12.000,00</td>
+        <td>Mie Ayam dengan berbagai menu <br>ada yang memakai paha dan juga sayap ayam</td>
+        <tr>
+          <td colspan="4"  align = "center"> <button  style="background-color:pink; color:black;" > ORDER</button> </td>
+        </tr>
+    </tr>
+    <tr class = "eh" align = "center">
+        <td><img src="juice.jpeg" width="200px" height="200px"></td>
+        <td>Juice</td>
+        <td>Rp.10.000,00</td>
+        <td>Menyediakan juice dengan berbagai rasa</td>
+        <tr>
+          <td colspan="4"  align = "center"> <button  style="background-color:pink; color:black;" > ORDER</button> </td>
+        </tr>
+    </tr>
+
+
+</table>
+</body>
+</html>
